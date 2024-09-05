1f59e8894725237616d5b68b5b342f95dfaff2d8
Jane Panov <panovjane@gmail.com>
2024-09-05 16:12:02 +0200

    Enable login with Spotify, get access token from logged in user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Login from "./components/Logins";
+import Spotify from "./components/Spotify";
+import { reducerCases } from "./utils/constants";
+import { useStateProvider } from "./utils/StateProvider";
 
 export default function App() {
-  return <div></div>;
+  const [{ token }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      console.log(token);
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
