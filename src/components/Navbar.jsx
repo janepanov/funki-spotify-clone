0bd59fd3e4a563a12becacd8cb8a8044129449c5
janepanov <panovjane@gmail.com>
2024-09-05 22:58:28 +0200

    List all tracks with their details from a default playlist inside the Body component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,8 @@ const Container = styled.div`
   position: sticky;
   top: 0;
   transition: 0.3s ease-in-out;
-  background-color: none;
+  background-color: ${({ navBackground }) =>
+    navBackground ? "rgba(0, 0, 0, 0.7)" : "none"};
   .search_bar {
     background-color: white;
     width: 30%;
