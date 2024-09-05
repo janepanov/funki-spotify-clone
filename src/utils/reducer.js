3e5086a6c7ae2c3b3bb612b5efbf44ec80541478
panceprendjov <phinkius@gmail.com>
2024-09-05 17:23:49 +0200

    Get current user's playlists and display them inside the Sidebar component

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -2,6 +2,7 @@ import { reducerCases } from "./constants";
 
 export const initialState = {
   token: null,
+  playlists: [],
 };
 
 const reducer = (state, action) => {
@@ -10,6 +11,13 @@ const reducer = (state, action) => {
       return {
         ...state,
         token: action.token,
+      };
+    }
+
+    case reducerCases.SET_PLAYLISTS: {
+      return {
+        ...state,
+        playlists: action.playlists,
       };
     }
     default:
