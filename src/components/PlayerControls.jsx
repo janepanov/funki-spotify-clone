d3ab4fc02fbc8ac6054694f6c1520d42be1713c2
janepanov <panovjane@gmail.com>
2024-09-06 01:11:31 +0200

    Enable going to previous or next song inside a playlist

---
+++ src/components/PlayerControls.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import {
+  BsFillPlayCircleFill,
+  BsFillPauseCircleFill,
+  BsShuffle,
+} from "react-icons/bs";
+import { CgPlayTrackNext, CgPlayTrackPrev } from "react-icons/cg";
+import { FiRepeat } from "react-icons/fi";
+import { useStateProvider } from "../utils/StateProvider";
+import { reducerCases } from "../utils/constants";
+import axios from "axios";
+
+export default function PlayerControls() {
+  const [{ token, playerState }, dispatch] = useStateProvider();
+  const [loading, setLoading] = useState(false);
+
+  const changeTrack = async (type) => {
+    setLoading(true);
+
+    try {
+      await axios.post(
+        `https://api.spotify.com/v1/me/player/${type}`,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      const pollInterval = 1000;
+      const maxRetries = 10;
+      let retryCount = 0;
+
+      while (retryCount < maxRetries) {
+        const response = await axios.get(
+          "https://api.spotify.com/v1/me/player/currently-playing",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+
+        if (response.data && response.data.item) {
+          const { item } = response.data;
+          const currentlyPlaying = {
+            id: item.id,
+            name: item.name,
+            artists: item.artists.map((artist) => artist.name),
+            image: item.album.images[2].url,
+          };
+
+          dispatch({ type: reducerCases.SET_PLAYING, currentlyPlaying });
+          dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
+          break;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, pollInterval));
+        retryCount++;
+      }
+    } catch (error) {
+      console.error("Error changing track:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Container>
+      <div className="shuffle">
+        <BsShuffle />
+      </div>
+      <div className="previous">
+        <CgPlayTrackPrev onClick={() => changeTrack("previous")} />
+      </div>
+      <div className="state">
+        {playerState ? <BsFillPauseCircleFill /> : <BsFillPlayCircleFill />}
+      </div>
+      <div className="next">
+        <CgPlayTrackNext onClick={() => changeTrack("next")} />
+      </div>
+      <div className="repeat">
+        <FiRepeat />
+      </div>
+      {loading && <div>Loading...</div>}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  svg {
+    color: #b3b3b3;
+    transition: 0.2s ease-in-out;
+    &:hover {
+      color: white;
+    }
+  }
+  .state {
+    svg {
+      color: white;
+    }
+  }
+  .previous,
+  .next,
+  .state {
+    font-size: 2rem;
+  }
+`;
